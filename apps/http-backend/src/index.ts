f8fe5eaf72df62775b568a39f5092d7745db49d1
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-05-30 23:52:08 +0530

    Imported zod schema in backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,17 +3,16 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { middleware } from "./middleware";
 dotenv.config();
-
 import {JWT_SECRET} from "@repo/backend-common/config";
 
-import {CreateUserSchema} from "@repo/common/types"
+import {CreateUserSchema , SigninSchema , CreateRoomSchema} from "@repo/common/types"
 
 
 
 const app=express();
 
 app.post("/signin",(req,res)=>{
-    const data=CreateUserSchema.safeParse(req.body);
+    const data=SigninSchema.safeParse(req.body);
     if(!data.success){
         res.json({
             message:"Incorrect inputs"
@@ -34,10 +33,27 @@ app.post("/signin",(req,res)=>{
 })
 
 app.post("/signup",(req,res)=>{
-    
+    const data=CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+    //db call
+    res.json({
+        userId:"123"
+    })
 })
 
 app.post("/room",middleware,(req,res)=>{
+    const data=CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
     // Database call
     res.status(200).json({
         roomId:"123"
