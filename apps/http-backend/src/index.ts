3551ee5163face883122acd53e3c8af49a387ec3
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-06-03 12:08:25 +0530

    Imported prismaclient in http-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,60 +3,60 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { middleware } from "./middleware";
 dotenv.config();
-import {JWT_SECRET} from "@repo/backend-common/config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
-import {CreateUserSchema , SigninSchema , CreateRoomSchema} from "@repo/common/types"
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
+import { prismaClient } from "@repo/db/client"
 
 
+const app = express();
 
-const app=express();
-
-app.post("/signin",(req,res)=>{
-    const data=SigninSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signin", (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
         res.json({
-            message:"Incorrect inputs"
+            message: "Incorrect inputs"
         })
         return;
     }
-    if(!JWT_SECRET){
+    if (!JWT_SECRET) {
         return;
     }
-    const userId=1
+    const userId = 1
     const token = jwt.sign({
         userId
-    },JWT_SECRET)
-    
+    }, JWT_SECRET)
+
     res.json({
         token
     })
 })
 
-app.post("/signup",(req,res)=>{
-    const data=CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
         res.json({
-            message:"Incorrect inputs"
+            message: "Incorrect inputs"
         })
         return;
     }
     //db call
     res.json({
-        userId:"123"
+        userId: "123"
     })
 })
 
-app.post("/room",middleware,(req,res)=>{
-    const data=CreateRoomSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/room", middleware, (req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
         res.json({
-            message:"Incorrect inputs"
+            message: "Incorrect inputs"
         })
         return;
     }
     // Database call
     res.status(200).json({
-        roomId:"123"
+        roomId: "123"
     })
 })
 
