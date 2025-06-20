1fe25b64c4ffba9866f6ed27414424ed56477ac4
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-06-20 22:57:05 +0530

    Restructuring drawing logic

--- apps/frontend/app/canvas/[roomId]/page.tsx
+++ apps/frontend/app/canvas/[roomId]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react"
 
 export default function Canvas() {
@@ -7,38 +8,9 @@ export default function Canvas() {
     useEffect(() => {
 
         if (canvasRef.current) {
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext("2d");
+            
 
-            if (!ctx) {
-                return;
-            }
-
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
-
-            canvas.addEventListener("mousedown", (e) => {
-                clicked = true;
-                startX = e.clientX
-                startY = e.clientY
-            })
-
-            canvas.addEventListener("mouseup", (e) => {
-                clicked = false;
-                console.log(e.clientX)
-                console.log(e.clientY)
-            })
-
-            canvas.addEventListener("mousemove", (e) => {
-                if (clicked) {
-                    const width = e.clientX-startX
-                    const height = e.clientY-startY
-                    ctx.clearRect(0,0,canvas.width,canvas.height)
-                    ctx.strokeRect(startX,startY,width,height);
-
-                }
-            })
+            initDraw(canvasRef.current)
 
         }
 
@@ -47,7 +19,7 @@ export default function Canvas() {
         <div>
 
             <div>
-                <canvas ref={canvasRef} width={1000} height={500}></canvas>
+                <canvas ref={canvasRef} width={500} height={500}></canvas>
             </div>
         </div>
     )
