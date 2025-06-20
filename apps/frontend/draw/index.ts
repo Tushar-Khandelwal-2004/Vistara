95ed97aff94c5fb34a5911de748715f5f7a838c0
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-06-20 23:44:40 +0530

    Now existing shapes remains on canvas

--- apps/frontend/draw/index.ts
+++ apps/frontend/draw/index.ts
@@ -1,7 +1,22 @@
 
+type Shape = {
+    type: "rect",
+    x: number,
+    y: number,
+    width: number,
+    height: number
+} | {
+    type: "circle",
+    centerX: number,
+    centerY: number,
+    radius: number
+}
+
 export function initDraw(canvas: HTMLCanvasElement) {
 
     const ctx = canvas.getContext("2d");
+
+    let existingShapes: Shape[] = [];
 
     if (!ctx) {
         return;
@@ -22,18 +37,37 @@ export function initDraw(canvas: HTMLCanvasElement) {
 
     canvas.addEventListener("mouseup", (e) => {
         clicked = false;
-        console.log(e.clientX)
-        console.log(e.clientY)
+        const width = e.clientX - startX
+        const height = e.clientY - startY
+        existingShapes.push({
+            type: "rect",
+            x: startX,
+            y: startY,
+            width: width,
+            height: height
+
+        })
     })
 
     canvas.addEventListener("mousemove", (e) => {
         if (clicked) {
             const width = e.clientX - startX
             const height = e.clientY - startY
-            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            clearCanvas(existingShapes,canvas,ctx);
             ctx.strokeRect(startX, startY, width, height);
 
         }
     })
 
+}
+
+function clearCanvas(existingShapes: Shape[], canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    existingShapes.map((shape) => {
+        ctx.strokeStyle = "black";
+        if (shape.type === "rect") {
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    })
 }
